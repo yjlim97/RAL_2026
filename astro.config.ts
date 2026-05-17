cdb72233aec1f2a11483d605712394161c35b0db
vdcl <dladydwo1234@gmail.com>
2026-05-17 03:53:26 -0400

    ok

--- astro.config.ts
+++ astro.config.ts
@@ -13,6 +13,9 @@ import astroExpressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://yjlim97.github.io",
+  base: "/RAL_2026/",
+
   vite: {
     plugins: [tailwindcss()],
   },
